201cdaef36e11b9106e434092567df56e2cf40c8
Pranav Sanvatsarkar <pranav.sanvatsarkar@enzigma.com>
2017-05-11 23:58:56 +0530

    -- implemented proper exception handling -- received command line arguments -- logged-in, searched, displayed search result count -- email message fields made accessible such as - To, From, CC, BCC, Subject -- configuration includes - email id, password and imap host address

--- imap-email-reader.py
+++ imap-email-reader.py
@@ -1,31 +1,78 @@
 import imaplib
 import configparser
+import sys
+import traceback
+import email
+import smtplib
+import argparse
 
-ORG_EMAIL = "@enzigma.com"
 FROM_EMAIL = ""
 FROM_PWD = ""
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT = 993
+SMTP_SERVER = ""
 
-def readEmail():
+def readEmail( key, value ):
 	try:
-		mail = imaplib.IMAP4_SSL(SMTP_SERVER)
-		rv, data = mail.login(FROM_EMAIL,FROM_PWD)
-		mail.select("inbox")
-		print ("Hello world!")
-		print ("rv" + rv)
-		print ("data" + str(data))
-	except e:
-		print ("Exception:" +  e)
+		imapClient = imaplib.IMAP4_SSL(SMTP_SERVER)
+		print ("\nLogging in...")
+		
+		imapClient.login(FROM_EMAIL,FROM_PWD)
+		print ("\nLog-in successfull!")
+		
+		imapClient.select("inbox")
+		print ("\nReading mailboxes...")
+		
+
+		if ( key == None or value == None ):
+			type, searchResult = imapClient.search( None, "ALL" )
+			print ("\nTotal search emails count:" + str(len(searchResult[0])))
+		
+		elif( key != None and value != None ):
+			type, searchResult = imapClient.search(None, "(" + key + " " + value + ")")
+			print ("\nTotal search emails count:" + str(len(searchResult[0])))
+		
+		for resultCount in searchResult[0].split():
+			typ, messages = imapClient.fetch(resultCount,"(RFC822)")
+			
+			for message in messages:
+				if isinstance(message, tuple):
+					msg = email.message_from_bytes(message[1])
+					email_to_id = msg["to"]
+					email_from_id = msg["from"]
+					email_cc_id = msg["cc"]
+					email_bcc_id = msg["bcc"]
+					email_subject = msg["subject"]
+					email_body = msg["body"]
+					print ("\nTo: " + str(email_to_id))
+					print ("\nFrom: " + str(email_from_id))
+					print ("\nCC: " + str(email_cc_id))
+					print ("\nBCC: " + str(email_bcc_id))
+					print ("\nSubject: " + str(email_subject))
+					print ("\nBody: " + str(email_body))
+				break
+
+	except Exception as e:
+		print ("\nException handled: " + str(e))
+		print ("\nException details:")
+		traceback.print_tb(e.__traceback__)
 
 if __name__ == "__main__":
 	try:
-		print ("Inside main")
 		config = configparser.ConfigParser()
 		config.read("configuration.ini")
 		config.sections()
+		
 		FROM_EMAIL = config["DEFAULT"]["from_email"]
 		FROM_PWD = config["DEFAULT"]["from_pwd"]
-		readEmail()
-	except e:
-		print ("Exception:" + e)
+		SMTP_SERVER = config["DEFAULT"]["smtp_server"]
+
+		parser = argparse.ArgumentParser(description="Please enter key and value for search..")
+		parser.add_argument("key",help="You need to specify the key to perform search on the emails. E.g. from, sent, etc")
+		parser.add_argument("value",help="You may optional specify the value for the key specified. If not specified, your key argument will not be considered")
+		args = parser.parse_args()
+		
+		readEmail(args.key, args.value)
+
+	except Exception as e:
+		print ("\nException handled: " + str(e))
+		print ("\nException details:")
+		traceback.print_tb(e.__traceback__)
